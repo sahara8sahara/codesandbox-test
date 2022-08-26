cb7f940b7a707f0f1a236c84f2c3c090be0f9906
sahara8sahara <61547459+sahara8sahara@users.noreply.github.com>
2022-08-26 20:27:14 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+console.log("asdf");
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<h2>テストです</h2>
+<div>
+We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
